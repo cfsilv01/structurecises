bf08d98527945b3946ba61775d905069f88d4abe
Christian Silva <cfsilva81@bellsouth.net>
2019-06-22 22:11:04 -0400

    code added to file

--- jp_problem.py
+++ jp_problem.py
@@ -13,3 +13,26 @@
         - start - initial player position/player next to eliminated player
         - table - array of players
 '''
+
+N=5
+M=0
+start = 0
+table = [i+1 for i in range(N)]
+
+def play_josephus(table, start):
+
+    if len(table) == 1:
+        print('Winner is ',table[0])
+    else:
+        # Index of next elimination
+        elim_player = (start + M) % len(table)
+        print('Player {} eliminated'.format(table[elim_player]))
+
+        # Remove eliminated players value at index
+        del table[elim_player]
+
+        # Recurse with updated array and index of next player
+        play_josephus(table, elim_player) 
+
+if __name__ == '__main__':
+    play_josephus(table, start)
