1dfde6d1a7c1c87a43a340ec9dac1fe463293c55
Christian Silva <cfsilva81@bellsouth.net>
2019-06-22 21:46:33 -0400

    create josephus file

---
+++ jp_problem.py
@@ -0,0 +1,15 @@
+'''
+    Josephus problem
+    
+    1 to N numbered players, are sitting in a circle.
+    Starting at person 1, after M passes, the person at Mth
+    pass is eliminated, the circle is reduced, and the game 
+    continues with the person next to the eliminated person. 
+    The last remaining person wins.
+
+    Variables
+        - N - number of players
+        - M - number of passes
+        - start - initial player position/player next to eliminated player
+        - table - array of players
+'''
